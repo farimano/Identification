6d541769e7e2e0df1efaada90152cc01670540b1
farimano <farimanez@gmail.com>
2021-09-08 02:19:09 +0200

    The outputs were fixed

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ import os
 from os.path import dirname, abspath
 from flask import Flask, request, render_template, flash
 from werkzeug.utils import secure_filename
+import json
+import time
 
 
 def create_app(test_config=None):
@@ -40,6 +42,35 @@ def create_app(test_config=None):
 		is_dot = '.' in name
 		is_allow = name.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 		return is_dot and is_allow
+	
+	# Save the selected id_event as json file
+	def save_json(id_event, cursor):
+		cursor.execute(
+			"""
+			SELECT e.error, e.error_description, e.recognition_degree,
+			e.number_faces, u.name, p.title, p.id_pos
+			FROM events e
+			LEFT JOIN users u
+			ON u.id_user = e.id_user
+			LEFT JOIN positions p
+			ON u.id_pos = p.id_pos
+			WHERE e.id_event = ?""", 
+			(id_event,)
+		)
+		row = cursor.fetchone()
+		e, e_d, r_d, f_n, n, t, i_p = row
+		if e:
+			error_data = {'description':e_d}
+		else:
+			error_data = False
+		result_data = {'N_faces':f_n, 'recognition_details':{'recognition_degree':r_d, 'threshold':0.6}}
+		employ_data = {'name':n, 'position':{'id_pos':i_p, 'title':t} }
+		data = {'error':error_data, 'result':result_data, 'employer':employ_data}
+		
+		pth = os.path.join(cur_dir, f"db/json/{id_event}.json")
+		with open(pth, "w") as write_file:
+			json.dump(data, write_file)
+		
 	
 	# The "body" of app
 	@app.route("/", methods=["GET", "POST"])
@@ -83,21 +114,29 @@ def create_app(test_config=None):
 			# Check whether user_id is in database or not
 			cursor.execute('SELECT * FROM users WHERE id_user=?', (user_id,))
 			if not len(cursor.fetchall()):
-				error = "This id does not exist in database"
+				error = "This id does not exist in database."
 				cursor.execute("UPDATE events SET (error, error_description)=(?, ?) WHERE id_event=?", (1, error, event_id))
+				save_json(event_id, cursor)
 				con.commit()
-				return render_template('base.html', out=(error, distance))
+				return render_template('base.html', out=error)
 			orig_path = os.path.join(app.config['ORIG_FOLDER'], f"{user_id}.jpg")
 			
 			error, distance, face_num = face_recog(orig_path, check_path)
-			if distance is not None:
+			if error is None:
 				# With threshold 0.6 this recognition test has 99.13% accuracy
 				result = int(distance<0.6)
-				cursor.execute("UPDATE events SET (distance, result, error)=(?, ?, ?) WHERE id_event=?", (distance, result, 0, event_id))
+				if not result:
+					error = "Fail. This person is not the id-card holder."
+					cursor.execute("UPDATE events SET (recognition_degree, error, error_description, number_faces)=(?, ?, ?, ?) WHERE id_event=?", (distance, 1, error, face_num, event_id))
+				else:
+					cursor.execute("UPDATE events SET (recognition_degree, error, number_faces)=(?, ?, ?) WHERE id_event=?", (distance, 0, face_num, event_id))
 			else:
-				cursor.execute("UPDATE events SET (error, error_description)=(?, ?) WHERE id_event=?", (1, error, event_id))
+				cursor.execute("UPDATE events SET (error, error_description, number_faces)=(?, ?, ?) WHERE id_event=?", (1, error, face_num, event_id))
+			save_json(event_id, cursor)
 			con.commit()
-			return render_template('base.html', out=(error, distance))
+			if error:
+				time.sleep(5)
+			return render_template('base.html', out=error or "The idendification has been finished successfully!")
 		return render_template('base.html', out=None)
 	
 	# Close the existed connection of database
--- app/schema.sql
+++ app/schema.sql
@@ -1,20 +1,34 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS positions;
 
 CREATE TABLE users (
 	id_user INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT
+	name TEXT,
+	id_pos INTEGER
 );
 
 CREATE TABLE events (
 	id_event INTEGER PRIMARY KEY AUTOINCREMENT,
 	id_user INTEGER NOT NULL,
-	distance NUMERIC(7, 2),
-	result INTEGER,
+	recognition_degree NUMERIC(7, 2),
+	number_faces INTEGER,
 	error INTEGER,
 	error_description TEXT
 );
 
-/* This is test values for dataset user  */
-INSERT INTO users (name) VALUES ("E");
-INSERT INTO users (name) VALUES ("L");
+CREATE TABLE positions (
+	id_pos INTEGER PRIMARY KEY AUTOINCREMENT,
+	title TEXT
+);
+
+/* This is test values for table positions  */
+INSERT INTO positions (title) VALUES ("Developer");
+INSERT INTO positions (title) VALUES ("HR-manager");
+INSERT INTO positions (title) VALUES ("Security");
+
+
+/* This is test values for table users  */
+INSERT INTO users (name, id_pos) VALUES ("Alice", 1);
+INSERT INTO users (name, id_pos) VALUES ("Viktor", 2);
+INSERT INTO users (name) VALUES ("Vladimir");
